745e617dc5d01e5ef61eb8c1a28d773c9d82ad8f
nazmul1994 <nazmulalorupa@gmail.com>
2021-03-22 18:58:21 -0400

    new

---
+++ TestNg/src/test/java/com/RegressionTest.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.testng.annotations.Test;
+
+public class RegressionTest extends basetest {
+	 @Test(groups= {"smoke"})
+	  public void test2() {
+		 System.out.println("test2");
+		 
+	  }
+	 @Test
+	  public void test3() {
+		 System.out.println("test3");
+		 
+	  }
+
+}
---
+++ TestNg/src/test/java/com/Seleniummethod.java
@@ -0,0 +1,7 @@
+package com;
+
+public class Seleniummethod {
+	public void multipleWidowHandle() {
+  
+	}
+}
